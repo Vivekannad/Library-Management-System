48ec8a08a389f03228af9807ad2b38a7ce419587
Vivek Anand <dhevikchanintha@gmail.com>
2025-03-10 23:02:22 +0500

    images field added in db and logic implemented

--- model/book.model.js
+++ model/book.model.js
@@ -18,6 +18,10 @@ const bookSchema = new Schema({
     publishedOn : {
         type : Date , 
         required : true
+    },
+    image: {
+        type: String,
+        required: true
     }
 })
 
--- router/addBook.router.js
+++ router/addBook.router.js
@@ -1,17 +1,30 @@
 import Router from "express"
+const router = Router();
 import { Book } from "../model/book.model.js";
 import { Author } from "../model/author.model.js";
 import { Category } from "../model/category.model.js";
-const router = Router();
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/my-uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 router.get("/", (req,res) => {
     if(!req.user) return res.redirect("/auth/login");
     return res.render("addBook");
 })
 
-router.post("/",   async(req,res) => {
-    const {title , category , author , date} = req.body;
-    if(!title || !category || !author ||!date){
+router.post("/",upload.single("file") ,   async(req,res) => {
+    const {title , category , author , date, file} = req.body;
+    if(!title || !category || !author ||!date || !file ){
         return res.redirect("/add");
     }
     try{
@@ -22,18 +35,17 @@ router.post("/",   async(req,res) => {
             });
         }
         let categoryDoc = await Category.findOne({category:category});
-        console.log(authorDoc)
         if(categoryDoc <= 0){
             categoryDoc = await Category.create({
                 category : category
             });
         }
-        console.log(categoryDoc)
         await Book.create({
             title : title , 
             category : categoryDoc._id, 
             author : authorDoc._id,
-            publishedOn : date
+            publishedOn : date,
+            image : `/my-uploads/${req.file.filename}`
         })
         return res.redirect("/");
     }catch(err){
